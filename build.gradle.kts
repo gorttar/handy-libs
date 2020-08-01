1dbc04123fd5ad6671bcbd88924d4f66b0c26480
Andrey Antipov <andrey.antipov@mynd.co>
2020-08-02 02:34:08 +0400

    configure maven central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,36 @@
-group = "com.github.gorttar"
-version = "1.0-SNAPSHOT"
+val javaLangVersion = "${JavaVersion.VERSION_1_8}"
+val libName = rootProject.name
 
-val kotlinGroup = "org.jetbrains.kotlin"
-val javaLangVersion = "1.8"
-val kotlinLangVersion = "1.3.72"
+val publicationName = "mavenJava"
+val publishToCentral = true
+val isRelease = true
+
+group = "com.github.gorttar"
+version = "1.0.0${"".takeIf { isRelease } ?: "-SNAPSHOT"}"
 
 plugins {
     java
     id("idea")
     kotlin("jvm") version "1.3.72"
+    `maven-publish`
+    `java-library`
+    signing
 }
 
-repositories { mavenCentral() }
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
 
 dependencies {
-    implementation(group = kotlinGroup, name = "kotlin-stdlib-jdk8", version = kotlinLangVersion)
-    implementation(group = kotlinGroup, name = "kotlin-reflect", version = kotlinLangVersion)
-    implementation(group = kotlinGroup, name = "kotlin-test", version = kotlinLangVersion)
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+    implementation(kotlin("test"))
     implementation(group = "com.willowtreeapps.assertk", name = "assertk-jvm", version = "0.20")
 
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = "5.6.2")
@@ -25,7 +39,10 @@ dependencies {
 }
 
 buildscript {
-    repositories { mavenCentral() }
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
     dependencies {
         classpath(group = "org.junit.jupiter", name = "junit-jupiter-api", version = "5.6.2")
         classpath(group = "org.jetbrains.kotlin", name = "kotlin-gradle-plugin", version = "1.3.72")
@@ -50,5 +67,69 @@ tasks {
         }
     }
 
+    javadoc {
+        if (JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+        }
+    }
+
     wrapper { gradleVersion = "6.5.1" }
 }
+
+val snapshotUrl = "https://oss.sonatype.org/content/repositories/snapshots"
+    .takeIf { publishToCentral }
+    ?: "$buildDir/repos/snapshots"
+val releaseUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+    .takeIf { publishToCentral }
+    ?: "$buildDir/repos/releases"
+
+publishing {
+    publications {
+        create<MavenPublication>(publicationName) {
+            groupId = "$group"
+            artifactId = libName
+            from(components["java"])
+            pom {
+                name.set("Handy libraries")
+                description.set("Bunch of somewhat usable libraries without any particular direction")
+                url.set("https://github.com/gorttar/$libName")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/gorttar/$libName/blob/master/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("gorttar")
+                        name.set("Andrey Antipov")
+                        email.set("gorttar@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/gorttar/$libName.git")
+                    developerConnection.set("scm:git:git://github.com/gorttar/$libName.git")
+                    url.set("https://github.com/gorttar/$libName")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = (releaseUrl.takeIf { isRelease } ?: snapshotUrl).let(::uri)
+            publishToCentral.takeIf { it }?.also {
+                repositories {
+                    val nexusUsername: String by project
+                    val nexusPassword: String by project
+
+                    credentials {
+                        username = nexusUsername
+                        password = nexusPassword
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing { sign(publishing.publications[publicationName]) }
